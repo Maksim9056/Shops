ffd59f885c6793cdb8e3379a8a11aea4cce10284
Maks <bobreczovm@inbox.ru>
2024-10-10 19:14:18 +0300

    Правки теперь InteractiveAuto буду переносить страницы

--- Shops.Client/Program.cs
+++ Shops.Client/Program.cs
@@ -7,7 +7,6 @@ namespace Shops.Client
         static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
             await builder.Build().RunAsync();
         }
     }
