8a79d9871ad1eb6f9e43aed1d2ba57ec80ac953c
Maks <bobreczovm@inbox.ru>
2024-11-26 15:19:34 +0300

    Правки

--- Scripts/Start/run_category.sh
+++ Scripts/Start/run_category.sh
@@ -2,6 +2,13 @@
 
 CONFIG_FILE="/app/appsettings.json"
 
+echo "=== Переменные окружения ==="
+echo "PG_HOST=${PG_HOST}"
+echo "PG_PORT=${PG_PORT}"
+echo "PG_DATABASE=${PG_DATABASE}"
+echo "PG_USERNAME=${PG_USERNAME}"
+echo "PG_USERPASSWORD=${PG_USERPASSWORD}"
+
 # Проверяем наличие файла конфигурации
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "Файл конфигурации $CONFIG_FILE не найден!"
@@ -9,11 +16,13 @@ if [ ! -f "$CONFIG_FILE" ]; then
     exit 1
 fi
 
+echo "=== Содержимое $CONFIG_FILE ДО замены ==="
+cat "$CONFIG_FILE"
+
 # Проверяем и заменяем целевую строку
 TARGET_STRING="Host=localhost;Database=StoreShop;Username=postgres;Password=1"
 REPLACEMENT_STRING="Host=${PG_HOST};Database=${PG_DATABASE};Username=${PG_USERNAME};Password=${PG_USERPASSWORD}"
 
-# Проверяем, содержит ли файл целевую строку
 if grep -q "$TARGET_STRING" "$CONFIG_FILE"; then
     echo "Целевая строка найдена. Выполняется замена на актуальные параметры."
     sed -i "s#${TARGET_STRING}#${REPLACEMENT_STRING}#g" "$CONFIG_FILE"
@@ -21,8 +30,7 @@ else
     echo "Целевая строка не найдена. Замена не требуется."
 fi
 
-# Проверка результата замены
-echo "Содержимое $CONFIG_FILE после замены:"
+echo "=== Содержимое $CONFIG_FILE ПОСЛЕ замены ==="
 cat "$CONFIG_FILE"
 
 # Запускаем приложение
